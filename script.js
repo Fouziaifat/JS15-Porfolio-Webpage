539de7010f6dc13a731f74ff6895e8589e74e146
Fouzia ifat <105037033+Fouziaifat@users.noreply.github.com>
2023-03-02 23:27:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".multiple-text",{
+    strings: ["Web Developer", "Freelancer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+
+});
